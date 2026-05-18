a7ee3e7449bdcc65d64c13640bffa922e4f45093
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-05-18 18:05:22 +0700

    class seseorang polimorpi

---
+++ polimorpi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+public:
+    virtual void pesan() = 0;
+    // virtual void pesen(){
+    //    cout << "pesen dari seseorang" << endl;
+    //}
+};
